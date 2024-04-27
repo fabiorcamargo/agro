a8f15cb70a984dba3182ec70d957c8a744aff031
Fabio Camargo <fabiorcamargo@gmail.com>
2024-04-27 16:57:54 -0300

    up

--- routes/api.php
+++ routes/api.php
@@ -33,49 +33,48 @@ Route::post('/form_send', function (Request $request) {
 
     $data = json_decode($request->getContent(), true);
 
-
-    // Obtém os dados do corpo da requisição POST
-    $dados = $request->all();
-
-    // Converte os dados para uma string formatada
-    $dadosFormatados = json_encode($dados);
-
-    // Caminho onde o arquivo será salvo
-    $caminhoArquivo = storage_path('app/dados_formulario.txt');
-
-    // Salva os dados no arquivo de texto
-    file_put_contents($caminhoArquivo, $dadosFormatados);
-
     //dd($data['mautic.form_on_submit'][0]['submission']['results']);
     // Acesse o email
 
-    // $dados = [];
-    // if (isset($data['mautic.form_on_submit'])) {
-    //     $dados['agent'] = $data['mautic.form_on_submit'][0]['submission']['results']['agent'];
-    //     $dados['fbid'] = $data['mautic.form_on_submit'][0]['submission']['trackingId'];
-    //     $dados['email'] = hash('sha256', $data['mautic.form_on_submit'][0]['submission']['results']['email']);
-    //     $dados['fbp'] = $data['mautic.form_on_submit'][0]['submission']['results']['fbp'];
-    //     $dados['fbc'] = $data['mautic.form_on_submit'][0]['submission']['results']['fbc'];
-    //     $dados['cidade'] = hash('sha256', strtolower(str_replace(' ', '', iconv('UTF-8', 'ASCII//TRANSLIT', $data['mautic.form_on_submit'][0]['submission']['results']['cidade']))));
-    //     $dados['estado'] = hash('sha256', strtolower(str_replace(' ', '', iconv('UTF-8', 'ASCII//TRANSLIT', $data['mautic.form_on_submit'][0]['submission']['results']['estado']))));
-    //     $dados['ip'] = $data['mautic.form_on_submit'][0]['submission']['ipAddress']['ipAddress'];
-    //     $dados['url'] = $data['mautic.form_on_submit'][0]['submission']['referer'];
-    //     $dados['time'] = strtotime($data['mautic.form_on_submit'][0]['submission']['dateSubmitted']);
-    //     $dados['country'] = hash('sha256', 'br');
-    //     $dados['phone'] = hash('sha256', "55" .   preg_replace('/[^A-Za-z0-9]/', '', $dados['phone']));
+    //  // Obtém os dados do corpo da requisição POST
+    //  $dados = $request->all();
+
+    //  // Converte os dados para uma string formatada
+    //  $dadosFormatados = json_encode($dados);
+ 
+    //  // Caminho onde o arquivo será salvo
+    //  $caminhoArquivo = storage_path('app/dados_formulario.txt');
+ 
+    //  // Salva os dados no arquivo de texto
+    //  file_put_contents($caminhoArquivo, $dadosFormatados);
+
+    $dados = [];
+    if (isset($data['mautic.form_on_submit'])) {
+        $dados['agent'] = $data['mautic.form_on_submit'][0]['submission']['results']['agent1'];
+        $dados['fbid'] = $data['mautic.form_on_submit'][0]['submission']['trackingId'];
+        $dados['email'] = hash('sha256', $data['mautic.form_on_submit'][0]['submission']['results']['email']);
+        $dados['phone'] = $data['mautic.form_on_submit'][0]['submission']['results']['telefone'];
+        $dados['fbp'] = $data['mautic.form_on_submit'][0]['submission']['results']['fbp'];
+        $dados['fbc'] = $data['mautic.form_on_submit'][0]['submission']['results']['fbc1'];
+        $dados['cidade'] = hash('sha256', strtolower(str_replace(' ', '', iconv('UTF-8', 'ASCII//TRANSLIT', $data['mautic.form_on_submit'][0]['submission']['results']['cidade']))));
+        $dados['estado'] = hash('sha256', strtolower(str_replace(' ', '', iconv('UTF-8', 'ASCII//TRANSLIT', $data['mautic.form_on_submit'][0]['submission']['results']['estado']))));
+        $dados['ip'] = $data['mautic.form_on_submit'][0]['submission']['ipAddress']['ipAddress'];
+        $dados['url'] = $data['mautic.form_on_submit'][0]['submission']['referer'];
+        $dados['time'] = strtotime($data['mautic.form_on_submit'][0]['submission']['dateSubmitted']);
+        $dados['country'] = hash('sha256', 'br');
+        $dados['phone'] = hash('sha256', "55" .   preg_replace('/[^A-Za-z0-9]/', '', $dados['phone']));
 
 
 
 
-    //     $fb = new FacebookApi;
-    //     $response = $fb->lead2($dados);
-    // } else {
-    //     $response = 'Dados Externos ' . json_encode($data);
-    // }
+        $fb = new FacebookApi;
+        $response = $fb->lead2($dados);
+    } else {
+        $response = 'Dados Externos ' . json_encode($data);
+    }
 
 
-    // return $response;
-    return '';
+    return $response;
     //Mail::to(auth()->user()->email)->send(new welcome_mail(auth()->user()));
     //new welcome_mail(auth()->user());
 });
