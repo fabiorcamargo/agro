f1570fbda4dae0dc33a497a1bf1c7d17f9f6979f
Fabio Camargo <fabiorcamargo@gmail.com>
2024-05-01 13:43:57 -0300

    up

--- resources/views/components/banner1.blade.php
+++ resources/views/components/banner1.blade.php
@@ -1,4 +1,3 @@
-
 <div class="relative bg-white overflow-hidden">
     <div class="max-w-screen mx-auto">
 
@@ -20,14 +19,15 @@
                 <div class="sm:text-center lg:text-left">
                     <h2
                         class="text-4xl tracking-tight leading-10 font-extrabold text-white sm:text-5xl sm:leading-none md:text-6xl">
-                        {{request()->input('cidade') ? 'Vagas em ' . request()->input('cidade') : "" }} {{env('BANNER_SLOGAN1')}}
+                        {{-- {{request()->input('cidade') ? 'Vagas em ' . request()->input('cidade') : "" }} --}}
+                        {{env('BANNER_SLOGAN1')}}
                         <br class="xl:hidden" />
                         <span class="text-white">{{env('BANNER_SLOGAN2')}}</span>
                         <span class="text-green3 pe-0">Agronegócio</span>
                     </h2>
                     <p
                         class="my-8 text-base text-white sm:mt-5 sm:text-lg sm:max-w-xl sm:leading-7 sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                        {{env('BANNER_DESC')}}
+                        {{env('BANNER_DESC')}} {{request()->input('cidade') ? request()->input('cidade') : "Sua Cidade" }} {{" - "}} {{request()->input('uf') ? request()->input('uf') : "" }}
                     </p>
 
                     <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
@@ -48,33 +48,46 @@
             </div>
         </div>
     </div>
-    
+
     <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-full">
-        <img src="/BG Agro.jpg" alt="{{env('APP_NAME')}}" class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"/>
-        
+        <img src="/BG Agro.jpg" alt="{{env('APP_NAME')}}"
+            class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" />
+
     </div>
 </div>
 
 
-    <div class="mx-auto max-w-5xl  sm:px-6 m-[-20px] lg:px-8 ">
-      <div class="transition duration-500 hover:scale-105 lg:hover:scale-110 relative isolate overflow-hidden bg-green2 px-6 py-16 shadow-2xl sm:rounded-3xl sm:px-16 md:py-16 lg:flex lg:gap-x-20 lg:px-24 lg:py-0 z-30">
-        {{-- <svg viewBox="0 0 1024 1024" class="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0" aria-hidden="true">
-          <circle cx="512" cy="512" r="512" fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fill-opacity="0.8" />
-          <defs>
-            <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
-              <stop stop-color="#86198f" />
-              <stop offset="1" stop-color="#e0e7ff" />
-            </radialGradient>
-          </defs>
+<div class="mx-auto max-w-5xl  sm:px-6 m-[-20px] lg:px-8 ">
+    <div
+        class="transition duration-500 hover:scale-105 lg:hover:scale-110 relative isolate overflow-hidden bg-green2 px-6 py-16 shadow-2xl sm:rounded-3xl sm:px-16 md:py-16 lg:flex lg:gap-x-20 lg:px-24 lg:py-0 z-30">
+        {{-- <svg viewBox="0 0 1024 1024"
+            class="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
+            aria-hidden="true">
+            <circle cx="512" cy="512" r="512" fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fill-opacity="0.8" />
+            <defs>
+                <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+                    <stop stop-color="#86198f" />
+                    <stop offset="1" stop-color="#e0e7ff" />
+                </radialGradient>
+            </defs>
         </svg> --}}
         <div class="mx-auto text-center lg:mx-0 lg:flex-auto lg:py-10 lg:text-left">
-          <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">{{env('MARKETING1')}} {{request()->input('cidade') ? request()->input('cidade') : "Sua Cidade" }}</h2>
-          <p class="mt-6 text-lg leading-8 text-gray-300">{{env('MARKETING2')}}</p>
-          {{--<div class="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
-            <a href="#" class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Get started</a>
-            <a href="#" class="text-sm font-semibold leading-6 text-white">Learn more <span aria-hidden="true">→</span></a>
-          </div>--}}
+            <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">{{env('MARKETING1')}}
+                {{request()->input('cidade') ? request()->input('cidade') : "Sua Cidade" }} {{" - "}} {{request()->input('uf') ? request()->input('uf') : "" }}</h2>
+            <p class="mt-6 text-lg leading-8 text-gray-300">Para Jovens de 13 à 21 anos!</p>
+
+            <p class="mt-6 text-lg leading-8 text-gray-300">Introdução ao Agronegócio: Descubra a base de tudo e como você pode se destacar neste setor dinâmico.
+                Drone e Tecnologia, Contratos e Negociações, Saúde e Radiologia Animal, Pastagem e Plantio, Maquinário,
+                Técnica de Vendas e Importação/Exportação.</p>
+            {{--<div class="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+                <a href="#"
+                    class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Get
+                    started</a>
+                <a href="#" class="text-sm font-semibold leading-6 text-white">Learn more <span
+                        aria-hidden="true">→</span></a>
+            </div>--}}
         </div>
-      </div>
     </div>
-    
+
+   
+</div>
